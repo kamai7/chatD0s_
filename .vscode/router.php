e1fa1343171b720d39ae0115e3b631a24b9c9d1f
kamai7 <anaellecarre35@gmail.com>
2025-02-02 17:39:31 +0100

    ??

--- .vscode/router.php
+++ .vscode/router.php
@@ -22,8 +22,8 @@ if (php_sapi_name() == "cli-server") {
             <script src="https://cdn.socket.io/4.7.5/socket.io.min.js" integrity="sha384-2huaZvOR9iDzHqslqwpR87isEmrfxqyWOF7hr7BY6KG0+hVKLoEXMPUJw3ynWuhO" crossorigin="anonymous"></script>
             <script type="module">
                 window.addEventListener('DOMContentLoaded', (event) => {
-                const socket = io(`ws://localhost:5003`, { 
-                    auth: { token : "MWIzZWEyN2ItMDJhMi00ZWYxLWE0ZGEtY2EzOTQ2YzI4ZWNiMTczODc3MTkzOTY4Nw==aMc" },   
+                const socket = io(`ws://localhost:5004`, { 
+                    auth: { token : "ZTJlMTQ3MTctZjM2Ni00NWM4LWFhMDEtZjkxNDYzMGY3NWU2MTczODUxNDMxNjkxMA==aMc" },   
                     withCredentials: true,
                     reconnectionAttempts: 3
                 });
