c80bcfa6f6ba0a4713935757ad4add492e7efaf0
kamai7 <anaellecarre35@gmail.com>
2025-04-27 20:58:50 +0200

    +

--- web/js/main.js
+++ web/js/main.js
@@ -3,9 +3,9 @@
 addEventListener("DOMContentLoaded", async (event) => {
     nav_page = await load_nav();
 
-    button_terminal = await load_fragment("button", {"action" : f(), "link" : TERMINAL_ICON});
-    button_user = await load_fragment("button", {"action" : f(), "link" : USER_ICON});
-    button_settings = await load_fragment("button", {"action" : open_settings(), "link" : SETTINGS_ICON});
+    button_terminal = await load_fragment("button", {"action" : "f()", "link" : TERMINAL_ICON});
+    button_user = await load_fragment("button", {"action" : "f()", "link" : USER_ICON});
+    button_settings = await load_fragment("button", {"action" : "open_settings()", "link" : SETTINGS_ICON});
     insert_html(button_terminal, "header-button", "beforeend");
     insert_html(button_user, "header-button", "beforeend");
     insert_html(button_settings, "header-button", "beforeend");
