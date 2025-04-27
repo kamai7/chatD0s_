884eaa4c0bf223fbfdde5453a480608db8b8ba7b
kamai7 <anaellecarre35@gmail.com>
2025-04-27 19:23:27 +0200

    d

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,18 @@
 var nav_page;
+var button_terminal;
+var button_user;
+var button_settings;
+const TERMINAL_ICON = "img/ui/terminal.png";
+const USER_ICON = "img/illustrations/not-log-user.png";
+const SETTINGS_ICON = "img/ui/settings.png";
 
 // add nav bar after document load
 addEventListener("DOMContentLoaded", async (event) => {
     nav_page = await load_nav();
 
-    for()
+    button_terminal = await load_fragment("button.html", {"link" : TERMINAL_ICON});
+    button_user = await load_fragment("button.html", {"link" : USER_ICON});
+    button_settings = await load_fragment("button.html", {"link" : SETTINGS_ICON});
+    insert_html(nav_page, "main", "afterbegin");
 });
+
