83fb779afb3e30cd12e1a854e83fae466ecfbac0
kamai7 <anaellecarre35@gmail.com>
2025-04-29 12:49:59 +0200

    correction js

--- web/js/fonctions.js
+++ web/js/fonctions.js
@@ -24,6 +24,12 @@ function insert_html(elem, location_id, placement) {
     }
 }
 
+function reload(element_id){
+    var container = document.getElementById(element_id);
+    var content = container.innerHTML;
+    container.innerHTML= content;
+}
+
 
 function display_before(elemToDisplay) {
     var parentElement = elemToDisplay.parentNode;
@@ -39,15 +45,8 @@ async function open_chat(self, name) {
 
     var chat_window = await load_fragment("chat", {messages: chats});
     var window = await load_fragment("window", {title: name, content: chat_window});
-    insert_html(window, "workspace-content", "afterbegin");
+    insert_html(window, "workspace-content", "beforeend");
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    for(friend in FRIENDS) {
-        var elem = load_fragment("fragments/social_container.php");
-        document.getElementById("nav").insertAdjacentHTML("beforeend", elem.innerHTML);
-    }
-});
 
 //load elems:
 async function load_nav() {
@@ -62,5 +61,15 @@ async function load_nav() {
 
 async function open_settings() {
     var settings_page = await load_fragment("window",{title:"settings", content:":)"});
-    insert_html(settings_page, "workspace-content", "afterbegin");
+    insert_html(settings_page, "workspace-content", "beforeend");
+}
+
+async function open_terminal() {
+    var commands = "";
+    commands += await load_fragment("command", {content: "coucou"});
+    commands += await load_fragment("command", {content: "coucou"});
+
+    var terminal_window = await load_fragment("terminal", {content: commands});
+    var window = await load_fragment("window", {title: "CD0s_ command prompt", content: terminal_window});
+    insert_html(window, "workspace-content", "beforeend");
 }
