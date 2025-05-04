a43cd88abadc3ef0f4cbd048979ec9cd5cfd9b7d
kamai7 <anaellecarre35@gmail.com>
2025-05-04 13:49:59 +0200

    poo patches

--- web/js/fragments/fragment.js
+++ web/js/fragments/fragment.js
@@ -3,10 +3,11 @@ class Fragment {
      * Classe représentant un fragment HTML.
      * @param {String} frag_name - Le nom du fragment.
      */
-    constructor(frag_name) {
+    constructor(frag_name, elems_list = []) {
         this.id = Math.random().toString(36).substring(2); //todo id random
         this.dom_elem;
         this.frag_name = frag_name;
+        this.elems_list = elems_list;
     }
 
     /**
@@ -14,12 +15,17 @@ class Fragment {
      * @param {String} parent_id - L'id de l'élément parent dans lequel insérer le fragment.
      * @param {String} placement - La position d'insertion du fragment ('beforeend' par défaut).
      */
-    async insert(parent_id, placement = "beforeend"){
+    async insert(parent_id, placement = "beforeend") {
         var elem = await this.get_html();
+
         document.getElementById(parent_id).insertAdjacentHTML(placement, elem);
         this.dom_elem = document.getElementById(this.id);
         this.init();
+        for(var elem of this.elems_list){
+            await elem.insert(this.id + "-content");
+        }
     }
+    
 
     /**
      * Récupère le code HTML du fragment (selon l'attribut 'frag_name').
--- web/js/fragments/nav.js
+++ web/js/fragments/nav.js
@@ -1,23 +1,10 @@
 class Nav extends Fragment{
     constructor(friends) {
-        super("nav");
-        this.content = friends;
-    }
-
-    async init() {
-        for(var friend of this.content) {
-            friend.dom_elem = document.getElementById(friend.id);
-            await friend.init();
-        }
+        super("nav", friends);
     }
 
     async get_html() {
         var html = await this.get_fragment();
-        var friends_html = '';
-        for(var friend of this.content) {
-            friends_html += await friend.get_html();
-        }
-        html = html.replaceAll("{{content}}", friends_html);
         return html;
     }
 }
