a1ff29bc738b3851e83a207450b240b9688c0f63
Rag404 <tt.favnec.yt@gmail.com>
2025-05-04 02:48:33 +0200

    ajout classe Window

--- web/js/fragments/window.js
+++ web/js/fragments/window.js
@@ -1,26 +1,42 @@
 class Window extends Fragment {
-    constructor(title, content) {
+    /**
+     * Crée une nouvelle instance de la classe Window.
+     * @param {String} title - Le titre de la fenêtre.
+     * @param {Fragment} content - Le contenu de la fenêtre.
+     * @param {boolean} maximized - Si la fenêtre est maximisée ou non (false par défaut).
+     * @param {number} width - La largeur de la fenêtre (0 par défaut).
+     * @param {number} height - La hauteur de la fenêtre (0 par défaut).
+     */
+    constructor(title, content, maximized=false, width=0, height=0) {
         super("window");
         this.title = title;
         this.content = content;
 
+        this.maximized = maximized;
+        this.minimized = false;
+
         this.pos_left = 0;
         this.pos_top = 0;
-        this.width = 500;
-        this.height = 300;
-
-        this.maximized = false;
-        this.minimized = false;
+        this.width = width;
+        this.height = height;
     }
 
     async init() {
-        this.dom_elem = document.getElementById(this.id);
+        this.set_size(this.width, this.height);
 
-        const title = this.dom_elem.getElementsByClassName("window-title")[0];
-        title.addEventListener("mousedown", this.drag);
+        if (this.minimized) {
+            const retrieve_button = this.dom_elem.getElementsByClassName("window-title")[0];
+            retrieve_button.addEventListener("click", this.retrieve.bind(this));
+        } else {
+            const title = this.dom_elem.getElementsByClassName("window-title")[0];
+            title.addEventListener("mousedown", this.drag.bind(this));
 
-        const minimize_button = this.dom_elem.getElementsByClassName("window-minimize")[0];
-        minimize_button.addEventListener("click", this.minimize.bind(this));
+            const minimize_button = this.dom_elem.getElementsByClassName("window-minimize")[0];
+            minimize_button.addEventListener("click", this.minimize.bind(this));
+
+            const maximize_button = this.dom_elem.getElementsByClassName("window-maximize")[0];
+            maximize_button.addEventListener("click", this.toggle_maximized.bind(this));
+        }
 
         const close_button = this.dom_elem.getElementsByClassName("window-close")[0];
         close_button.addEventListener("click", this.close.bind(this));
@@ -29,8 +45,10 @@ class Window extends Fragment {
     async get_html() {
         var html = await this.get_fragment();
         html = html.replaceAll("{{title}}", this.title);
-        html = html.replaceAll("{{content}}", await this.content.get_html());
-        return html; 
+        if (!this.minimized) {
+            html = html.replaceAll("{{content}}", await this.content.get_html());
+        }
+        return html;
     }
 
     /**
@@ -64,36 +82,34 @@ class Window extends Fragment {
     drag(event) {
         this.focus();
 
-        const offsetX = event.clientX - this.dom_elem.offsetLeft;
-        const offsetY = event.clientY - this.dom_elem.offsetTop;
-
         const workspace = document.getElementById("workspace-content");
-        const nav = document.getElementById("nav");
 
-        const limitTop = document.getElementById("header")?.offsetHeight || 0;
-        const limitLeft = nav ? nav.offsetWidth : 0;
-        const workspaceWidth = workspace ? workspace.offsetWidth : window.innerWidth;
-        const workspaceHeight = workspace ? workspace.offsetHeight : window.innerHeight;
+        const mouseOffX = event.clientX - this.pos_left;
+        const mouseOffY = event.clientY - this.pos_top;
+        
+        // Récupère les positions min et max, en fonction de la taille du workspace
+        const limitRight = workspace.offsetWidth - this.dom_elem.offsetWidth;
+        const limitBottom = workspace.offsetHeight - this.dom_elem.offsetHeight;
 
-        function moveAt(e) {
-            const newLeft = Math.max(limitLeft, Math.min(e.clientX - offsetX, (limitLeft + workspaceWidth - this.dom_elem.offsetWidth)));
-            const newTop = Math.max(limitTop, Math.min(e.clientY - offsetY, (limitTop + workspaceHeight - this.dom_elem.offsetHeight)));
-            
-            if (e.clientY <= limitTop + 1){
-                this.set_maximized(true);
-            } else {
-                this.set_maximized(false);
-                this.set_pos(newLeft, newTop);
-            }
-        }
+        // Listener pour le mouvement de la souris
+        const moveListener = ((e) => {
+            const newLeft = this.pos_left + e.movementX;
+            const newTop = this.pos_top + e.movementY;
 
-        function stopMove() {
-            document.removeEventListener("mousemove", moveAt);
-            document.removeEventListener("mouseup", stopMove);
-        }
+            this.set_maximized(newTop <= 0);
+            this.set_pos(newLeft, newTop);
+        }).bind(this);
 
-        document.addEventListener("mousemove", moveAt);
-        document.addEventListener("mouseup", stopMove);
+        // Listener pour le relâchement du bouton de la souris
+        const stopListener = ((e) => {
+            document.removeEventListener("mousemove", moveListener);
+            document.removeEventListener("mouseup", stopListener);
+
+            this.set_pos(this.pos_left, Math.max(0, this.pos_top));
+        }).bind(this);
+
+        document.addEventListener("mousemove", moveListener);
+        document.addEventListener("mouseup", stopListener);
     }
 
     /**
@@ -101,58 +117,47 @@ class Window extends Fragment {
      * @param {boolean} bool - Si la fenêtre est maximisée ou non. 
      */
     set_maximized(bool) {
-        // Si la valeur actuelle est déjà celle demandée, ne rien faire.
-        if (this.maximized === bool) return;
-        
-        this.maximized = bool;
-
-        if (bool) {
-            this.dom_elem.style.width = "100%";
-            this.dom_elem.style.height = "100%";
-            this.dom_elem.style.left = "0px";
-            this.dom_elem.style.top = "0px";
-        } else {
-            this.dom_elem.style.width = `${this.width}px`;
-            this.dom_elem.style.height = `${this.height}px`;
-            this.dom_elem.style.left = `${this.pos_left}px`;
-            this.dom_elem.style.top = `${this.pos_top}px`;
+        // Si la valeur est différente de l'état actuel, on inverse l'état
+        if (this.maximized !== bool) {
+            this.maximized = bool;
+            this.dom_elem.classList.toggle("window-maximized");
         }
+    }
+
+    /**
+     * Listener qui inverse l'état de maximisation de la fenêtre.
+     * @param {MouseEvent} event - L'événement de la souris.
+     */
+    toggle_maximized(event) {
+        this.set_maximized(!this.maximized);
     }
 
     /**
      * Listener qui minimise la fenêtre.
      * @param {MouseEvent} event - L'événement de la souris.
      */
-    minimize(event) {
+    async minimize(event) {
         this.minimized = true;
-        this.dom_elem.classList.add("window-minimized");
+        this.frag_name = "window_minimized";
 
-        // On clone l'élément pour le réinsérer dans le DOM
-        var tempElement = this.dom_elem.cloneNode(true);
-        document.getElementById("workspace-windows").insertAdjacentElement("afterbegin", tempElement);
-        
-        // On enlève l'élément d'origine du DOM
+        // On remplace l'ancien élément par le nouvel élément mis au bon endroit
         this.dom_elem.remove();
-
-        // On récupère l'élément minimisé
-        this.dom_elem = document.getElementById(this.id);
-
-        /*var windowElement = elem.closest(".window");
-        var chat_name = windowElement.getElementsByClassName("window-title")[0].innerText;
-        windowElement.remove();
-    
-        var tempElement = document.createElement('div');
-        tempElement.innerHTML = await load_fragment("fragments/window_minimised.php");
-        tempElement.getElementsByClassName('window-title')[0].textContent = chat_name;
-        document.getElementById("workspace-windows").insertAdjacentHTML("afterbegin", tempElement.innerHTML);*/
+        await this.insert("workspace-windows", "afterbegin")
     }
 
     /**
      * Listener qui réouvre une fenêtre minimisée.
      * @param {MouseEvent} event - L'événement de la souris.
      */
-    retrieve(event) {
-        // TODO : Réouvrir la fenêtre minimisée
+    async retrieve(event) {
+        this.minimized = false;
+        this.frag_name = "window";
+
+        // On remplace l'ancien élément par le nouvel élément mis au bon endroit
+        this.dom_elem.remove();
+        await this.insert("workspace-content", "afterbegin");
+
+        this.set_pos(this.pos_left, this.pos_top);
     }
 
     /**
@@ -167,6 +172,6 @@ class Window extends Fragment {
      * Focus la fenêtre actuelle en la déplaçant au premier plan.
      */
     focus() {
-        this.dom_elem.appendChild(this.dom_elem);
+        this.dom_elem.parentNode.appendChild(this.dom_elem);
     }
 }
