d8dd770d0ce11f69155e2a4b8771f4d2220b80d4
kamai7 <anaellecarre35@gmail.com>
2025-05-06 22:32:59 +0200

    javad

--- web/js/fragments/message.js
+++ web/js/fragments/message.js
@@ -1,4 +1,12 @@
 class Message extends Fragment {
+
+    /**
+     * Crée une nouvelle instance de la classe Message
+     * permet de créer des messages a afficher
+     * @param {String} pp_link Lien de la photo de profil
+     * @param {String} username Nom d'utilisateur de la personne envoyant le message
+     * @param {String} content Contenu du message
+     */
     constructor(pp_link, username, content) {
         super("message");
         this.pp_link = pp_link;
@@ -6,6 +14,10 @@ class Message extends Fragment {
         this.content = content;
     }
 
+    /**
+     * Fonction qui permet de récupérer le code HTML du message
+     * @returns {String} le code HTML du message
+     */
     async get_html(){
         var html = await super.get_fragment();
         html = html.replaceAll("{{pp_link}}", this.pp_link);
